2f931ea9bcfbe475bfc2747ff661a423ccad11db
meir1997 <149312502+meir1997@users.noreply.github.com>
2026-07-31 10:40:06 +0300

    Add Firebase project configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.RETZEF_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB2lHRJy8k6t3m1zSLry7Y96jQ5GgPpXLU",
+  authDomain: "retzef-habit-tracker.firebaseapp.com",
+  projectId: "retzef-habit-tracker",
+  storageBucket: "retzef-habit-tracker.firebasestorage.app",
+  messagingSenderId: "537414766473",
+  appId: "1:537414766473:web:97eccd92f8ba91f4bedb58",
+};
