3c046dacf06d8c870cf9d50fdaa52af47c0cec4b
meir1997 <149312502+meir1997@users.noreply.github.com>
2026-08-19 11:52:57 +0300

    Refresh cache for reading tracker

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "retzef-v16";
+const CACHE_NAME = "retzef-v17";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=6",
+  "./styles.css?v=7",
   "./firebase-config.js?v=1",
   "./firebase-sync.js?v=2",
-  "./app.js?v=13",
+  "./app.js?v=14",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./assets/icon-180.png",
