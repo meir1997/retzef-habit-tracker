53dbd6337f29ea195a314216465e3b0f08ed0e01
meir1997 <149312502+meir1997@users.noreply.github.com>
2026-08-29 23:04:05 +0300

    Refresh cache for bonus habits

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "retzef-v28";
+const CACHE_NAME = "retzef-v29";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=11",
+  "./styles.css?v=12",
   "./firebase-config.js?v=1",
   "./firebase-sync.js?v=3",
-  "./app.js?v=23",
+  "./app.js?v=24",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./assets/icon-180.png",
