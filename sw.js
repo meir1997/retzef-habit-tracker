6c9e5bd3cefae153b3f31f66cd8cbc9dc2e7ecf4
meir1997 <149312502+meir1997@users.noreply.github.com>
2026-09-03 09:37:57 +0300

    Refresh mobile cloud-load cache

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "retzef-v36";
+const CACHE_NAME = "retzef-v37";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=12",
   "./firebase-config.js?v=1",
   "./firebase-sync.js?v=6",
-  "./app.js?v=30",
+  "./app.js?v=31",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./assets/icon-180.png",
